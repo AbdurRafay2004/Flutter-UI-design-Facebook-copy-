b272cd2f998a90411e4ca8b41f0b37fb02582d5b
Tahsin2004 <rafay.mail2004@gmail.com>
2024-01-23 01:14:55 +0600

    fb tabs done

--- lib/main.dart
+++ lib/main.dart
@@ -12,28 +12,88 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-            toolbarHeight: 40,
-            actions: const [
-            CircleAvatar(
-              backgroundColor: Color.fromARGB(255, 232, 232, 232),
-              child: Icon(Icons.search, color: Colors.black),
+            appBar: AppBar(
+              backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+              toolbarHeight: 60,
+              actions: const [
+                //search icon
+                CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 232, 232, 232),
+                  radius: 25,
+                  child: Icon(Icons.search, color: Colors.black),
+                ),
+                SizedBox(width: 10), // Add some spacing between icons
+                //chat icon
+                CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 232, 232, 232),
+                  radius: 25,
+                  child: Icon(Icons.chat, color: Colors.black),
+                ),
+                SizedBox(width: 15)
+
+                ///dont know any alternative
+              ],
+              title: const Text(
+                "Facebook",
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-            SizedBox(width: 10), // Add some spacing between icons
-            CircleAvatar(
-              backgroundColor: Color.fromARGB(255, 232, 232, 232),
-              child: Icon(Icons.chat, color: Colors.black),
-            ),
-            SizedBox(width: 20)///dont know any alternative
-            ],
-            title: const Text("Facebook",
-            style: TextStyle(
-              color: Colors.blue,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-          ),
-        ));
+            //bg color
+            body: Container(
+                color: const Color.fromARGB(255, 226, 226, 226),
+                child: Column(
+                  children: [
+                    //row of fb tabs?
+                    Container(
+                      color: Colors.white,
+                      height: 60,
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Icon(
+                            Icons.home,
+                            color: Color.fromARGB(255, 124, 124, 124),
+                            size: 35,
+                          ),
+                          Icon(
+                            Icons.smart_display,
+                            color: Color.fromARGB(255, 124, 124, 124),
+                            size: 35,
+                          ),
+                          Icon(
+                            Icons.event_note,
+                            color: Color.fromARGB(255, 124, 124, 124),
+                            size: 35,
+                          ),
+                          Icon(
+                            Icons.groups,
+                            color: Color.fromARGB(255, 124, 124, 124),
+                            size: 35,
+                          ),
+                          Icon(
+                            Icons.notifications_active,
+                            color: Color.fromARGB(255, 124, 124, 124),
+                            size: 35,
+                          ),
+                          Icon(
+                            Icons.menu,
+                            color: Color.fromARGB(255, 124, 124, 124),
+                            size: 35,
+                          ),
+                        ],
+                      ),
+                    )
+
+                    //whats on your mind section
+
+                    //3 buttons, photo , feeling, location
+
+                    //stories section in Row
+                  ],
+                ))));
   }
 }
